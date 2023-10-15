d0f7427a90c2734f28d45c65feb71afb12dc7623
chaerla <chelgchen@gmail.com>
2023-10-15 11:29:22 +0700

    feat: add Coordinate class

---
+++ src/lib/Coordinate.java
@@ -0,0 +1,19 @@
+package lib;
+
+public class Coordinate {
+    private int row;
+    private int col;
+
+    public Coordinate(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+}
