16931589f97a6877b1788dda9b951ada0b85a33a
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-10-18 01:06:54 +0700

    feat: node tree class for reservation tree

---
+++ src/lib/NodeTree.java
@@ -0,0 +1,31 @@
+package lib;
+
+import javax.swing.tree.TreeNode;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NodeTree {
+    private double value;
+    private NodeTree parent;
+    private Map<Coordinate, NodeTree> children;
+
+    public NodeTree(double value) {
+        this.value = value;
+        this.children = new HashMap<>();
+    }
+
+    public NodeTree(double value, NodeTree parent) {
+        this.value = value;
+        this.parent = parent;
+        this.children = new HashMap<>();
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void addChildren(double value, Coordinate coordinate) {
+        NodeTree children = new NodeTree(value, this);
+        this.children.put(coordinate, children);
+    }
+}
