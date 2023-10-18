84c980eefda787ecaaf4d6ddf4db23099efd51ee
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-10-18 17:34:12 +0700

    feat: time based depth cutoff

--- src/lib/MinimaxBot.java
+++ src/lib/MinimaxBot.java
@@ -1,11 +1,16 @@
 package lib;
 
+import javafx.scene.DepthTest;
+
 import java.util.List;
 
 public class MinimaxBot extends Bot {
-    final int MAX_DEPTH = 5;
-
-    private int visitCount = 0;
+    private int maxDepth = -1;
+    private long depthEstimatedVisits = 1;
+    private final int PRUNING_FACTOR = 5000;
+    private final int TIME_PER_VISIT = 50; // microseconds
+    private long visitCount = 0;
+    private int initialEmptyCnt = 0;
 
     public MinimaxBot(Player player) {
         super(player);
@@ -13,7 +18,11 @@ public class MinimaxBot extends Bot {
 
     @Override
     public Coordinate findBestMove(Board currentBoard) {
-//        System.out.println("===== BOT'S TURN =====");
+        initialEmptyCnt = currentBoard.getEmptyCoordinates().size();
+        depthEstimatedVisits = 1;
+        maxDepth = -1;
+        cutoff(currentBoard, 0);
+
         int v = Integer.MIN_VALUE;
 
         List<Coordinate> possibleMoves = currentBoard.getEmptyCoordinates();
@@ -22,18 +31,38 @@ public class MinimaxBot extends Bot {
 
         this.visitCount = 0;
 
-        for (Coordinate move : possibleMoves) {
-            Board successor = new Board(currentBoard);
-            successor.updateCells(move.getRow(), move.getCol(), this.player);
-            int successorValue = minValue(successor, nextPlayer, v, Integer.MAX_VALUE, 1);
-            if (successorValue > v) {
-                v = successorValue;
-                bestMove = move;
-            }
-        }
+        long startTime = System.nanoTime();
+        long timeout = 3_500_000_000L; // 3.5 seconds
 
-//        System.out.println("===== VISIT COUNT =====");
-//        System.out.println(visitCount);
+        try {
+            for (Coordinate move : possibleMoves) {
+                Board successor = new Board(currentBoard);
+                successor.updateCells(move.getRow(), move.getCol(), this.player);
+                int successorValue = minValue(successor, nextPlayer, v, Integer.MAX_VALUE, 1);
+                if (successorValue > v) {
+                    v = successorValue;
+                    bestMove = move;
+                }
+
+                long currentTime = System.nanoTime();
+                if (currentTime - startTime > timeout) {
+                    throw new RuntimeException("Search may exceed 5 seconds and was terminated early.");
+                }
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000;
+        double durationInSeconds = duration / 1000000.0;
+
+        System.out.println("===== VISIT COUNT =====");
+        System.out.println("Visit Count: " + visitCount);
+        System.out.printf("Duration: %.2f seconds%n", durationInSeconds);
+        System.out.println("Empty Cells: " + currentBoard.getEmptyCoordinates().size());
+        if (visitCount != 0)
+            System.out.println("Time per visit: " + (duration / visitCount) + " micro second");
+        System.out.println("MAX DEPTH: " + maxDepth);
 
         return bestMove;
     }
@@ -76,8 +105,29 @@ public class MinimaxBot extends Bot {
     }
 
     private boolean cutoff(Board state, int depth) {
-        if (state.getEmptyCoordinates().size() == 0) return true;
-        return depth >= MAX_DEPTH;
+        if (state.getEmptyCoordinates().size() == 0) {
+            maxDepth = depth;
+            return true;
+        }
+        if (maxDepth == -1) {
+            depth++;
+//            System.out.println("==== DEPTH ==== ");
+            int emptyCells = initialEmptyCnt - depth + 1;
+            depthEstimatedVisits = depthEstimatedVisits * emptyCells;
+            long estVisits = depthEstimatedVisits / PRUNING_FACTOR;
+            long estTime = estVisits * TIME_PER_VISIT;
+//            System.out.println("Empty Cells: " + emptyCells);
+//            System.out.println("Depth est visits: " + depthEstimatedVisits);
+//            System.out.println("Est visits: " + estVisits);
+//            System.out.println("Est time: " + estTime);
+            if (estTime > 5 * 1e6) {
+                maxDepth = depth - 1;
+                return true;
+            }
+            return false;
+        } else {
+            return depth >= maxDepth;
+        }
     }
 
     private int evaluate(Board state) {
